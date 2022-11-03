2358f9bcb6c66d42ac217ad7a7507de8b2d8af70
Lucas-Veras <verasluca00@gmail.com>
2022-11-03 10:15:57 -0300

    arvore AVL

--- arvoreAVL/ArvoreAVLTeste.java
+++ arvoreAVL/ArvoreAVLTeste.java
@@ -8,17 +8,17 @@ public class ArvoreAVLTeste {
 		arvore.insert(1);
 		arvore.insert(2);
 		arvore.insert(3);
-		arvore.insert(4);
-		arvore.insert(5);
+	//	arvore.insert(4);
+	/*	arvore.insert(5);
 		arvore.insert(6);
 		arvore.insert(7);
 		arvore.insert(8);
 		arvore.insert(9);
-		arvore.insert(10);
+		arvore.insert(10);*/
 		arvore.mostraArvore();
-		arvore.remove(1);
+		//arvore.remove(1);
 		arvore.mostraArvore();
-		arvore.remove(3);
+	//	arvore.remove(3);
 		arvore.mostraArvore();
 		
 	}
